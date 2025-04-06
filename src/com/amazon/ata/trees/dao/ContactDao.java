a4354ac50637326b59748a4c55d582d3c67b6604
Cybers911 <info@cybers911.com>
2025-04-06 15:51:53 -0500

    sol1 1

--- src/com/amazon/ata/trees/dao/ContactDao.java
+++ src/com/amazon/ata/trees/dao/ContactDao.java
@@ -6,6 +6,7 @@ import com.amazon.ata.trees.model.SortBy;
 import com.amazon.ata.trees.model.SortOrder;
 
 import java.util.SortedMap;
+import java.util.TreeMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -23,12 +24,14 @@ public class ContactDao {
 
     @Inject
     public ContactDao() {
-        this.contactsSortedByFirstName = null;
-        this.contactsSortedByLastName = null;
+        this.contactsSortedByFirstName = new TreeMap<>(Comparator.comparing(Name::getFirstName));
+
+        this.contactsSortedByLastName = new TreeMap<>(Comparator.comparing(Name::getLastName));
     }
 
     /**
      * Saves the given contact.
+     *
      * @param contact to add.
      * @return The added contact.
      */
@@ -41,13 +44,28 @@ public class ContactDao {
     /**
      * Retrieves all contacts in the provided sort by order. If no sort parameters are given, returns the default
      * of sorting by last name in ascending order.
-     * @param sortBy Attribute to sort contacts by, e.g. first name, last name.
+     *
+     * @param sortBy    Attribute to sort contacts by, e.g. first name, last name.
      * @param sortOrder order to return contacts, either ascending or descending.
      * @return map of name to contacts in requested sorted order.
      */
     public SortedMap<Name, Contact> getContacts(SortBy sortBy, SortOrder sortOrder) {
         // TODO implement
-        return null;
+        //sortby
+        SortedMap<Name, Contact> baseMap = null;
+        if (sortBy == SortBy.FIRST_NAME) {
+            baseMap = contactsSortedByFirstName;
+        } else if (sortBy == SortBy.LAST_NAME) {
+            baseMap = contactsSortedByLastName;
+        }
+        //sorted order
+        if (sortOrder == SortOrder.DESCENDING) {
+            return new TreeMap<>(baseMap).descendingMap();
+        } else {
+            return new TreeMap<>(baseMap);
+        }
+
+
     }
 
     /**
@@ -62,15 +80,40 @@ public class ContactDao {
      * Retrieves a portion of the contacts whose Name is equal to or after the given startKey based on the sorting
      * parameters.
      *
-     * @param startKey The Name that should be the first contact in the returned Map
-     * @param sortBy Attribute to sort contacts by, e.g. first name, last name.
+     * @param startKey  The Name that should be the first contact in the returned Map
+     * @param sortBy    Attribute to sort contacts by, e.g. first name, last name.
      * @param sortOrder order to return contacts, either ascending or descending.
      * @return portion of contacts that includes startKey and all contacts after startKey based on requested sorted
      * order.
      */
     public SortedMap<Name, Contact> getContactsStartingAt(Name startKey, SortBy sortBy, SortOrder sortOrder) {
         // TODO implement
-        return null;
-    }
+    /*    public SortedMap<Name, Contact> getContactsStartingAt(Name startKey, SortBy sortBy, SortOrder sortOrder) {
+            // TODO implement
+            SortedMap<Name, Contact> baseMap = null;
+            if (sortBy == SortBy.FIRST_NAME) {
+                baseMap = contactsSortedByFirstName;
+            } else if (sortBy == SortBy.LAST_NAME) {
+                baseMap = contactsSortedByLastName;
+            }
+            SortedMap<Name, Contact> resultMap = new TreeMap<>();
+            for (Map.Entry<Name, Contact> entry : baseMap.entrySet()) {
+                if (entry.getKey().compareTo(startKey) >= 0) {
+                    resultMap.put(entry.getKey(), entry.getValue());
+                } else {
+                    break;
+                }
+            }
+            if (sortOrder == SortOrder.DESCENDING) {
+                return new TreeMap<>(resultMap).descendingMap();
+            } else {
+                return resultMap;
+            }
+            return null;
+        }*/
 
+        return getContacts(sortBy, sortOrder).tailMap(startKey);
+
+
+    }
 }
